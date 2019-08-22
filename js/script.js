2e2e543a86888bb64b2e27dae42df49fd52bd0ad
Jenna Tate <jennahtate@gmail.com>
2019-08-21 20:50:30 -0400

    Code quality fixed using linters, beautification applied

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-function menuToggle () {
-    var x = document.getElementById('myNavToggle');
-    if (x.className === 'navToggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navToggle';
-    }
+// eslint-disable-next-line no-unused-vars
+function menuToggle() {
+  // eslint-disable-next-line no-var
+  var x = document.getElementById('myNavToggle');
+  if (x.className === 'navToggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navToggle';
   }
+}
